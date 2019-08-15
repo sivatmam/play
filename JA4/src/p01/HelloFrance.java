871c5211178844469460530cc0c8f7a22714e76b
Sivatma Maharaj <32339882+sivatmam@users.noreply.github.com>
2019-08-15 11:45:27 -0400

    Add files via upload

---
+++ JA4/src/p01/HelloFrance.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package p01;
+
+/**
+ *
+ * @author x194951
+ */
+public class HelloFrance {
+    
+    public static void main(String args[]){
+        
+        System.out.println("Hello France!");
+        
+    }
+}
