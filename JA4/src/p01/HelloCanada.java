e3c9c3a33dfe75d998e2826c852c5171995624c3
Sivatma Maharaj <32339882+sivatmam@users.noreply.github.com>
2019-08-15 11:40:31 -0400

    Add files via upload

---
+++ JA4/src/p01/HelloCanada.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package p01;
+
+/**
+ *
+ * @author x194951
+ */
+public class HelloCanada {
+    
+    public static void main(String args[]){
+        
+        System.out.println("Hello Canada!");
+        
+    }
+    
+}
